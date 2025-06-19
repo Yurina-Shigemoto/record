bc53409d736eb95196000642157ff64e48636863
Yurina-Shigemoto <kawaisora522@gmail.com>
2025-06-19 15:05:55 +0900

    Add files via upload

---
+++ レコード大賞/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace RecordingApp
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button startRecordingButton;
+        private System.Windows.Forms.Button stopRecordingButton;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Timer timer1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startRecordingButton = new System.Windows.Forms.Button();
+            this.stopRecordingButton = new System.Windows.Forms.Button();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // startRecordingButton
+            // 
+            this.startRecordingButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.startRecordingButton.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.startRecordingButton.Location = new System.Drawing.Point(63, 191);
+            this.startRecordingButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.startRecordingButton.Name = "startRecordingButton";
+            this.startRecordingButton.Size = new System.Drawing.Size(159, 81);
+            this.startRecordingButton.TabIndex = 0;
+            this.startRecordingButton.Text = "録音開始！";
+            this.startRecordingButton.UseVisualStyleBackColor = false;
+            this.startRecordingButton.Click += new System.EventHandler(this.startRecordingButton_Click);
+            // 
+            // stopRecordingButton
+            // 
+            this.stopRecordingButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.stopRecordingButton.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.stopRecordingButton.Location = new System.Drawing.Point(267, 191);
+            this.stopRecordingButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.stopRecordingButton.Name = "stopRecordingButton";
+            this.stopRecordingButton.Size = new System.Drawing.Size(158, 81);
+            this.stopRecordingButton.TabIndex = 1;
+            this.stopRecordingButton.Text = "ストーップ！";
+            this.stopRecordingButton.UseVisualStyleBackColor = false;
+            this.stopRecordingButton.Click += new System.EventHandler(this.stopRecordingButton_Click);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.timeLabel.Location = new System.Drawing.Point(135, 75);
+            this.timeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(200, 100);
+            this.timeLabel.TabIndex = 2;
+            this.timeLabel.Text = "00:00:00";
+            this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 337);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.stopRecordingButton);
+            this.Controls.Add(this.startRecordingButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MainForm";
+            this.Text = "録音するよ！";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ レコード大賞/Form1.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Lame;
+using System.Diagnostics;
+
+namespace RecordingApp
+{
+    public partial class MainForm : Form
+    {
+        private WaveInEvent waveIn;
+        private LameMP3FileWriter writer;
+        private Stopwatch stopwatch;
+        private SaveFileDialog saveFileDialog;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startRecordingButton_Click(object sender, EventArgs e)
+        {
+            saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "MP3 Files|*.mp3";
+            saveFileDialog.Title = "Save an MP3 File";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                waveIn = new WaveInEvent();
+                waveIn.WaveFormat = new WaveFormat(44100, 1);
+                waveIn.DataAvailable += OnDataAvailable;
+                writer = new LameMP3FileWriter(saveFileDialog.FileName, waveIn.WaveFormat, LAMEPreset.VBR_90);
+                waveIn.StartRecording();
+
+                stopwatch = new Stopwatch();
+                stopwatch.Start();
+                timer1.Start();
+            }
+        }
+
+        private void stopRecordingButton_Click(object sender, EventArgs e)
+        {
+            waveIn.StopRecording();
+            writer.Dispose();
+            waveIn.Dispose();
+            stopwatch.Stop();
+            timer1.Stop();
+        }
+
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            writer.Write(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = stopwatch.Elapsed;
+            timeLabel.Text = String.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
+        }
+    }
+}
